3f9cd57a0d94043b1b28373091922ce94af4f2c7
NahuelKliphan <49241891+NahuelKliphan@users.noreply.github.com>
2019-08-20 10:06:56 -0300

    Listo

---
+++ calc.js
@@ -0,0 +1,67 @@
+
+var a=0;
+var b=0;
+var total=0;
+var op="" ;
+
+
+function multiplicar() {
+    document.getElementById("visor").value = "";
+    a=0;
+    b=0;
+    total=0;
+    op="" ;
+
+}
+
+function operador(o) {
+    op = o;
+    b = parseInt(b);
+    b = a;
+    a = 0;
+    document.getElementById("visor").value += o;
+}
+
+function resultado() {
+
+    switch (op) {
+        case '+':
+
+            var total = parseInt(b) + parseInt(a);
+
+            break;
+            case '-':
+
+            var total = parseInt(b) - parseInt(a);
+
+            break;
+            case '/':
+
+            var total = parseInt(b) / parseInt(a);
+
+            break;
+            case 'x':
+
+            var total = parseInt(b) * parseInt(a);
+
+            break;
+    }
+
+
+
+    document.getElementById("visor").value = total;
+     a= total;
+
+
+    
+ 
+
+}
+
+function agregarNumero(n) {
+
+    a = a + n;
+
+    document.getElementById("visor").value += n;
+
+}
